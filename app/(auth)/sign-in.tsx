ecd24c3070ca6abf7345b49cdcc6faf69d12cc37
NoobCoder_69 <vishant448@gmail.com>
2024-08-19 17:34:31 +0530

    Onboarding and signup page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 const singIn = () => {
   return (
     <SafeAreaView>
@@ -10,5 +10,3 @@ const singIn = () => {
 };
 
 export default singIn;
-
-const styles = StyleSheet.create({});
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,14 +1,65 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
-
+import { ScrollView, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, useSafeAreaFrame } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import InputField from "@/components/InputField";
+import { icons } from "@/constants";
+import CustomButton from "@/components/CutomButton";
+import { Link } from "expo-router";
 const signUp = () => {
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+
+  const onSignupPress = async () => {};
   return (
-    <SafeAreaView>
-      <Text>signUp</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            textContentType="emailAddress"
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            textContentType="password"
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignupPress}
+            className="mt-6"
+          />
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Login</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default signUp;
-
-const styles = StyleSheet.create({});
